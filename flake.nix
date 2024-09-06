430d74eddc3e73a92199ad43d6cb1b54e149367d
TornaxO7 <tornax@proton.me>
2024-09-06 18:02:32 +0200

    init project

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+
+        perSystem = { self', lib, system, pkgs, config, ... }: {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              tree-sitter
+              nodePackages_latest.nodejs
+            ];
+          };
+        };
+      };
+}
